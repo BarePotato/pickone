864b7149535adc2532276c83e9fac0f2e0ebf9ae
_Bare <bare@logixsr.com>
2021-10-10 06:11:19 -0400

    replace find-fd command call with dir walker. colorize output

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
+use std::fs;
+
 use rand::{thread_rng, Rng};
 
 fn main() {
-    let boobs = std::process::Command::new("fd").arg("-d").arg("1").arg("-t").arg("d").output().unwrap();
-
-    let mut dirs = boobs.stdout.split(|b| b == &b'\n').map(|b| std::str::from_utf8(b).unwrap()).collect::<Vec<&str>>();
-
-    if let Some(last) = dirs.last() {
-        if last.is_empty() {
-            dirs.pop();
-        }
-    }
+    let dirs = match get_dirs() {
+        Ok(d) => d,
+        Err(e) => return print_error(&e.to_string()),
+    };
 
     if dirs.len().lt(&1) {
-        return println!("There needs to be at least one directory in target location.");
+        return print_error("There needs to be at least one directory in target location.");
     }
 
     let mut rng = thread_rng();
     let i = rng.gen_range(0..dirs.len());
 
-    println!("{}", dirs[i]);
+    println!("\x1b[1;35m{}\x1b[0m", dirs[i]);
+}
+
+fn get_dirs() -> Result<Vec<String>, std::io::Error> {
+    Ok(fs::read_dir(".")?
+        .filter_map(|p| p.ok())
+        .filter(|p| p.path().is_dir())
+        .map(|p| p.file_name().into_string())
+        .filter_map(|p| p.ok())
+        .collect::<Vec<String>>())
+}
+
+fn print_error(e: &str) {
+    println!("\x1b[1;31m{}\x1b[0m", e);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dir() {
+        // This is going to fail anywhere other than my original cargo crate
+        assert_eq!(get_dirs().unwrap(), vec!["src", ".git", "target"]);
+    }
 }
